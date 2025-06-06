89521ac4a6587c70f189a2a02733148beff3c4a8
Odu-Enkay <[erozonankiru@gmail.com]>
2025-06-06 07:49:28 -0600

    implement Add to cart test cases

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -3,6 +3,11 @@ describe("Home Page", () => {
     cy.visit("/");
     cy.get(".products article").should("be.visible");
   });
+
+  it("There is 2 products on the page", () => {
+    cy.visit("/");
+    cy.get(".products article").should("have.length", 2);
+  });
 });
 
 
--- cypress/e2e/product_details.cy.js
+++ cypress/e2e/product_details.cy.js
@@ -4,13 +4,12 @@ describe("Product Details Navigation", () => {
   });
 
   it("navigates to a product detail page when clicking a product", () => {
-    // Click on the first product article inside the products container
     cy.get(".products article").first().click();
 
-    // After clicking, assert the URL includes /products/ (or whatever your detail path is)
+   
     cy.url().should("include", "/products/");
 
-    // check that product detail content is visible, e.g. product name or description
+  
     cy.get(".product-detail").should("be.visible");
   });
 });
