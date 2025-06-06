2dfe3e8585f85a78ff5f8506760f5c2a81fda3d2
Odu-Enkay <[erozonankiru@gmail.com]>
2025-06-06 06:36:49 -0600

    modified user session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,8 @@
 class SessionsController < ApplicationController
+  
+  def new
+    
+  end
 
   def create
    #user = User.find_by_email(params[:email])
@@ -8,7 +12,8 @@ class SessionsController < ApplicationController
     session[:user_id] = user.id
     redirect_to '/'
    else
-    redirect_to '/login'
+    flash[:error] = 'Invalid email or password'
+    render :new
    end 
   end
     
